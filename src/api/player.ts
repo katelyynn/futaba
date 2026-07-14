56d25d7f537705b4b18f21479579de37d008f2be
marin <46572320+katelyynn@users.noreply.github.com>
2026-07-14 18:30:32 +0100

    fix: weird playback state

--- src/api/player.ts
+++ src/api/player.ts
@@ -120,8 +120,15 @@ function setupEvents() {
 
     let index = currentIndex + 1;
     if (index >= queue.length) {
-      if (loop == true) index = 0;
-      else return;
+      if (loop == true) {
+        index = 0;
+      } else {
+        // the engine schedules the same song again even if loop is off
+        // this is a safety measure for now to ensure no weirdness
+        console.error("Audio: cancelled playback as queue is over", currentIndex, index, queue.length);
+        Player.stop();
+        return;
+      }
     }
     if (loop == "once") index = currentIndex;
 
